5160a4b043b7bd3e316fc5f563d88baa642d25ff
cementix <bodakondrackij@gmail.com>
2025-11-24 15:54:30 +0100

    task 5

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tsp_parse import parse_tsp_df
-from distance import city_distance
+from utils import city_distance, calculate_fitness
 import random
 
 df1 = parse_tsp_df("./tsps/berlin52.tsp")
@@ -13,18 +13,30 @@ city1 = df1.loc[df1.id == 1].iloc[0]
 city2 = df1.loc[df1.id == 2].iloc[0]
 
 dist = city_distance(city1, city2)
-print(dist)
+print("distance between city 1 and 2:", dist)
+
 
 def create_random_solution(df):
-    cities = df['id'].tolist()
+    cities = df["id"].tolist()
     random.shuffle(cities)
     return cities
 
-solution = create_random_solution(df2)
-print("random solution: ", solution)
 
-ids = df2['id'].tolist()
+solution = create_random_solution(df2)
+print("random solution:", solution)
+
+ids = df2["id"].tolist()
 if sorted(solution) == sorted(ids):
     print("all cities included, no duplicates")
 else:
-    print("something wrong(missing or duplicate ids)")
+    print("something wrong (missing or duplicate ids)")
+
+test_solution = create_random_solution(df2)
+fitness = calculate_fitness(df2, test_solution)
+print("test solution:", test_solution)
+print("fitness:", fitness)
+
+if fitness > 0:
+    print("fitness seems valid (positive value)")
+else:
+    print("fitness error: distance must be positive")
---
+++ utils.py
@@ -0,0 +1,21 @@
+import math
+
+
+def city_distance(city1, city2):
+    x1, y1 = city1["x"], city1["y"]
+    x2, y2 = city2["x"], city2["y"]
+
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def calculate_fitness(df, solution):
+    total = 0.0
+
+    city_map = {row.id: row for _, row in df.iterrows()}
+
+    for i in range(len(solution)):
+        c1 = city_map[solution[i]]
+        c2 = city_map[solution[(i + 1) % len(solution)]]
+        total += city_distance(c1, c2)
+
+    return total
